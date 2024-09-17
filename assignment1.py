5748152063ddace8b089833761c8643d7e33d062
G. Koenders <163114003+Gilian2002@users.noreply.github.com>
2024-09-17 18:54:50 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,10 +1,10 @@
-
+#%%
 import statsmodels.formula.api as smf
 import pandas as pd
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
-#explore the data
+#%%
 data.head()
 
 #Select the data, - assign 1
@@ -15,12 +15,12 @@ modelFit = model.fit()
 modelFit.summary()
 print(modelFit.summary())
 
-#use the test_data
+#%%
 test_data = pd.read_csv('https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv')
 test_data = test_data[['hour']]
 test_data.head(10)
 
-#Predict
+#%%
 pred = modelFit.predict(test_data)
 
 print(pred)
