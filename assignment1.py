00609c733c1dc9b129db95090e65e4f9b49d3cf6
G. Koenders <163114003+Gilian2002@users.noreply.github.com>
2024-09-17 15:22:56 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -47,9 +47,9 @@ import plotly.graph_objects as go
 
 # Plotting our data
 
-smoothData = pd.DataFrame([employment.values, alpha020.fittedvalues.values,  alpha050.fittedvalues.values,  alpha080.fittedvalues.values]).T
+smoothData = pd.DataFrame([taxi.values, alpha020.fittedvalues.values,  alpha050.fittedvalues.values,  alpha080.fittedvalues.values]).T
 smoothData.columns = ['Truth', 'alpha=0.2', 'alpha=0.5', 'alpha=0.8']
-smoothData.index = employment.index
+smoothData.index = taxi.index
 
 fig = px.line(smoothData, y = ['Truth', 'alpha=0.2', 'alpha=0.5', 'alpha=0.8'],
         x = smoothData.index,
@@ -60,7 +60,7 @@ fig = px.line(smoothData, y = ['Truth', 'alpha=0.2', 'alpha=0.5', 'alpha=0.8'],
        )
 
 # Dynamically set x-axis and y-axis ranges
-fig.update_xaxes(range=[smoothData.index[-50], forecast020.index[-1]])
+fig.update_xaxes(range=[smoothData.index[-744], forecast020.index[-1]])
 fig.update_yaxes(range=[smoothData['Truth'].min() - 1000, smoothData['Truth'].max() + 1000])
 
 
@@ -70,3 +70,37 @@ fig.update_yaxes(range=[smoothData['Truth'].min() - 1000, smoothData['Truth'].ma
 fig.add_trace(go.Scatter(x=forecast020.index, y = forecast020.values, name='Forecast alpha=0.2', line={'color':'red'}))
 fig.add_trace(go.Scatter(x=forecast050.index, y = forecast050.values, name='Forecast alpha=0.5', line={'color':'green'}))
 fig.add_trace(go.Scatter(x=forecast080.index, y = forecast080.values, name='Forecast alpha=0.8', line={'color':'purple'}))
+
+# Linear trend
+model = ExponentialSmoothing(taxi, trend='add', seasonal='add').fit()
+# Linear trend with damping
+dampedModel = ExponentialSmoothing(taxi, trend='mul', seasonal='add', damped=True, use_boxcox=True).fit(use_brute=True) # Set use_boxcox during initialization
+
+forecast_t = model.forecast(744)
+forecast_dt = dampedModel.forecast(744)
+import plotly.graph_objects as go
+
+# Plotting our data
+
+smoothData = pd.DataFrame([taxi.values, model.fittedvalues.values, dampedModel.fittedvalues.values]).T
+smoothData.columns = ['Truth', 'Trend', 'Damped Model']
+smoothData.index = taxi.index
+
+fig = px.line(smoothData, y = ['Truth', 'Trend', 'Damped Model'], 
+        x = smoothData.index,
+        color_discrete_map={"Truth": 'blue',
+                           'Trend': 'red',
+                            'Damped Model': 'green'
+                           },
+              title='With Seasonality'
+       )
+
+# Dynamically set x-axis and y-axis ranges
+fig.update_xaxes(range=[smoothData.index[-744], forecast020.index[-1]])
+fig.update_yaxes(range=[smoothData['Truth'].min() - 1000, smoothData['Truth'].max() + 1000])
+
+
+# Incorporating the Forecasts
+
+fig.add_trace(go.Scatter(x=forecast_t.index, y = forecast_t.values, name='Forecast Trend', line={'color':'red'}))
+fig.add_trace(go.Scatter(x=forecast_dt.index, y = forecast_dt.values, name='Forecast Damped Model', line={'color':'green'}))
